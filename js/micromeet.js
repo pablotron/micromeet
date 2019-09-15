87ffd91789f384c34db71e7a64428ee9a4b81854
Paul Duncan <pabs@pablotron.org>
2019-09-14 23:34:12 -0400

    refactor js, add help page

--- js/micromeet.js
+++ js/micromeet.js
@@ -1,230 +1,314 @@
 (function() {
   "use strict";
 
-  var Times = (function() {
-    var TZ_OFS = (new Date()).getTimezoneOffset();
+  // micromeet namespace
+  var MM = {
+    /**
+     * event list namespace
+     */
+    Events: (function() {
+      var TZ_OFS = (new Date()).getTimezoneOffset();
 
-    function make_date(date, time) {
-      var r = new Date(date + 'T' + time + ':00');
-      return r;
-    }
+      function make_date(date, time) {
+        var r = new Date(date + 'T' + time + ':00');
+        return r;
+      }
 
-    function step(date, freq) {
-      var r = new Date(date.getTime() + (+freq * 1000));
-      return r;
-    }
+      function step(date, freq) {
+        var r = new Date(date.getTime() + (+freq * 1000));
+        return r;
+      }
 
-    function summarize(summary, num, max) {
-      return summary + ' (' + num + '/' + max + ')';
-    }
+      function summarize(summary, num, max) {
+        return summary + ' (' + num + '/' + max + ')';
+      }
 
-    function make(data) {
-      var r = [],
-          min = make_date(data.date, data.min),
-          max = make_date(data.date, data.max);
+      function make(data) {
+        var r = [],
+            min = make_date(data.date, data.min),
+            max = make_date(data.date, data.max);
 
-      for (var t = min; t < max; t = step(t, data.freq)) {
-        r.push({
-          min: t,
-          max: step(t, +data.freq - 1)
+        for (var t = min; t < max; t = step(t, data.freq)) {
+          r.push({
+            min: t,
+            max: step(t, +data.freq - 1)
+          });
+        }
+
+        return r.map(function(row, i) {
+          row.summary = summarize(data.summary, (i + 1), r.length);
+          row.description = summarize(data.description, (i + 1), r.length);
+          return row;
         });
       }
 
-      return r.map(function(row, i) {
-        row.summary = summarize(data.summary, (i + 1), r.length);
-        row.description = summarize(data.description, (i + 1), r.length);
-        return row;
-      });
-    }
-
-    return {
-      make: make,
-    };
-  })();
-
-  var Views = {
-    CSV: (function() {
-      var HEADER = [
-        'start time',
-        'end time',
-        'summary',
-        'description',
-        'organizer name',
-        'organizer email',
-      ];
-
-      function quote(s) {
-        return '"' + (s || '').replace(/"/g, '""') + '"';
-      }
-
-      function add_event(rows, data, ev) {
-        rows.push([
-          ev.min,
-          ev.max,
-          quote(ev.summary),
-          quote(ev.description),
-          quote(data.name),
-          quote(data.email),
-        ].join(','));
-      }
-
-      function make(data, times) {
-        // header
-        var rows = [].concat(HEADER.join(','));
-
-        // append events
-        for (var i = 0; i < times.length; i++) {
-          add_event(rows, data, times[i]);
-        }
-
-        // build/return result
-        return rows.join("\n");
-      }
-
+      // expose public interface
       return {
         make: make,
       };
     })(),
 
-    ICS: (function() {
-      var HEADER = [
-        'BEGIN:VCALENDAR',
-        'VERSION:2.0',
-        'PRODID:-//pmdn/micromeet//NONSGML v1.0//EN',
-      ];
+    /**
+     * rendering view namespace
+     */
+    Views: {
+      /**
+       * csv renderer
+       */
+      CSV: (function() {
+        var HEADER = [
+          'start time',
+          'end time',
+          'summary',
+          'description',
+          'organizer name',
+          'organizer email',
+        ];
 
-      var FOOTER = [
-        'END:VCALENDAR',
-      ];
-
-      function make_time(d) {
-        // convert to short iso8601 date format
-        return d.toISOString().replace(/:|-|\.\d{3}/g, '');
-      }
-
-      function add_event(rows, data, ev) {
-        rows.push(
-          'BEGIN:VEVENT',
-          'UID:uid-' + make_time(ev.min) + '@micromeet.pmdn.org',
-          'ORGANIZER;CN=' + data.name + ':MAILTO:' + data.email,
-          'ORGANIZER;CN=' + data.name + ':MAILTO:' + data.email,
-          'DTSTART:' + make_time(ev.min),
-          'DTEND:' + make_time(ev.max),
-          'SUMMARY:' + ev.summary,
-          'DESCRIPTION:' + ev.description,
-          'SEQUENCE:1',
-          'END:VEVENT'
-        );
-      }
-
-      function make(data, times) {
-        // header
-        var rows = [].concat(HEADER);
-
-        // append events
-        for (var i = 0; i < times.length; i++) {
-          add_event(rows, data, times[i]);
+        function quote(s) {
+          return '"' + (s || '').replace(/"/g, '""') + '"';
         }
 
-        // append footer
-        rows = rows.concat(FOOTER);
+        function add_event(rows, data, ev) {
+          rows.push([
+            ev.min,
+            ev.max,
+            quote(ev.summary),
+            quote(ev.description),
+            quote(data.name),
+            quote(data.email),
+          ].join(','));
+        }
 
-        // build/return result
-        return rows.join("\n");
-      }
+        function make(data, evs) {
+          // header
+          var rows = [].concat(HEADER.join(','));
 
-      return {
-        make: make,
+          // append events
+          for (var i = 0; i < evs.length; i++) {
+            add_event(rows, data, evs[i]);
+          }
+
+          // build/return result
+          return rows.join("\n");
+        }
+
+        return {
+          make: make,
+        };
+      })(),
+
+      /**
+       * ics renderer
+       */
+      ICS: (function() {
+        var HEADER = [
+          'BEGIN:VCALENDAR',
+          'VERSION:2.0',
+          'PRODID:-//pmdn/micromeet//NONSGML v1.0//EN',
+        ];
+
+        var FOOTER = [
+          'END:VCALENDAR',
+        ];
+
+        function make_time(d) {
+          // convert to short iso8601 date format
+          return d.toISOString().replace(/:|-|\.\d{3}/g, '');
+        }
+
+        function add_event(rows, data, ev) {
+          rows.push(
+            'BEGIN:VEVENT',
+            'UID:uid-' + make_time(ev.min) + '@micromeet.pmdn.org',
+            'ORGANIZER;CN=' + data.name + ':MAILTO:' + data.email,
+            'ORGANIZER;CN=' + data.name + ':MAILTO:' + data.email,
+            'DTSTART:' + make_time(ev.min),
+            'DTEND:' + make_time(ev.max),
+            'SUMMARY:' + ev.summary,
+            'DESCRIPTION:' + ev.description,
+            'SEQUENCE:1',
+            'END:VEVENT'
+          );
+        }
+
+        function make(data, evs) {
+          // header
+          var rows = [].concat(HEADER);
+
+          // append events
+          for (var i = 0; i < evs.length; i++) {
+            add_event(rows, data, evs[i]);
+          }
+
+          // append footer
+          rows = rows.concat(FOOTER);
+
+          // build/return result
+          return rows.join("\n");
+        }
+
+        return {
+          make: make,
+        };
+      })(),
+    },
+
+    /**
+     * delay: Return a function that is delayed for the given number of
+     * milliseconds.
+     *
+     * Note: the timeout will reset if the function is called within the
+     * delay interval.
+     */
+    delay: function(time, fn) {
+      var timeout = null;
+
+      return function() {
+        if (timeout !== null) {
+          // clear existing timeout
+          clearTimeout(timeout);
+        }
+
+        // set new timeout
+        timeout = setTimeout(function() {
+          fn();
+        }, time);
       };
-    })(),
-  };
+    },
 
-  /**
-   * delay: Return a function that is delayed for the given number of
-   * milliseconds.
-   *
-   * Note: the timeout will reset if the function is called within the
-   * delay interval.
-   */
-  function delay(time, fn) {
-    var timeout = null;
+    /**
+     * iterate over list (potentially a NodeList).
+     */
+    each: function(list, fn) {
+      Array.prototype.forEach.call(list, fn);
+    },
 
-    return function() {
-      if (timeout !== null) {
-        // clear existing timeout
-        clearTimeout(timeout);
-      }
-
-      // set new timeout
-      timeout = setTimeout(function() {
-        fn();
-      }, time);
-    };
-  }
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var INPUTS = document.querySelectorAll('input, select'),
-        ELS = [{
-          id:   'num',
-          css:  '.num-meetings',
-        }, {
-          id:   'ics',
-          css:  '.download-btn[data-type="ics"]',
-        }, {
-          id:   'csv',
-          css:  '.download-btn[data-type="csv"]',
-        }].reduce(function(r, row) {
-          r[row.id] = document.querySelectorAll(row.css);
-          return r;
-        }, {});
-
-    function get_data() {
-      return Array.prototype.reduce.call(INPUTS, function(r, el) {
+    /**
+     * build hash of parameters from elements.
+     */
+    get_data: function(els) {
+      return Array.prototype.reduce.call(els, function(r, el) {
         r[el.id] = el.value;
         return r;
       }, {});
+    },
+
+    /**
+     * check for required inputs.
+     */
+    is_valid: function(data) {
+      return (data.min && data.max && data.freq);
+    },
+
+    /**
+     * used to build map of keys to node lists.
+     */
+    ELS: [{
+      id:   'num',
+      css:  '.num-meetings',
+    }, {
+      id:   'download',
+      css:  '.download-btn',
+    }, {
+      id:   'ics',
+      css:  '.download-btn[data-type="ics"]',
+    }, {
+      id:   'csv',
+      css:  '.download-btn[data-type="csv"]',
+    }],
+  };
+
+  // cached data (used by download button click handlers)
+  var CACHE = {};
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // build element map
+    var ELS = MM.ELS.reduce(function(r, row) {
+      r[row.id] = document.querySelectorAll(row.css);
+      return r;
+    }, { fields: document.querySelectorAll('input, select') });
+
+    // enable/disable download buttons
+    function set_btns(enabled) {
+      // disable download buttons
+      MM.each(ELS.download, function(el) {
+        if (enabled) {
+          el.classList.remove('disabled');
+          el.disabled = false;
+        } else {
+          el.classList.add('disabled');
+          el.disabled = true;
+        }
+      });
     }
 
-    var refresh = delay(100, function() {
+    // reset state
+    function reset() {
       // reset output
-      Array.prototype.forEach.call(ELS.num, function(el) {
+      MM.each(ELS.num, function(el) {
         el.innerHTML = '0';
       });
 
-      // get input parameters
-      var data = get_data();
+      // disable download buttons
+      set_btns(false);
+    }
 
-      // check inputs
-      if (!data.min || !data.max || !data.freq) {
+    // refresh handler (called by input change/keydown handlers)
+    var refresh = MM.delay(100, function() {
+      // reset output
+      reset();
+
+      // get input data and validate it
+      var data = MM.get_data(ELS.fields);
+      if (!MM.is_valid(data)) {
         return;
       }
 
-      // make time range
-      var times = Times.make(data);
+      // enable download buttons
+      set_btns(true);
 
-      // refresh meeting length and meeting count
-      Array.prototype.forEach.call(ELS.num, function(el) {
-        el.innerHTML = times.length;
+      // cache data, build array of events
+      CACHE.data = data;
+      CACHE.evs = MM.Events.make(data);
+
+      // refresh meeting count
+      MM.each(ELS.num, function(el) {
+        el.innerHTML = CACHE.evs.length;
       });
 
-      // set calendar download button
-      var ics = btoa(Views.ICS.make(data, times));
-      Array.prototype.forEach.call(ELS.ics, function(el) {
+      // set ics btn download names
+      MM.each(ELS.ics, function(el) {
         el.download = data.summary + '.ics';
-        el.href = 'data:text/calendar;base64,' + ics;
       });
 
-      var csv = btoa(Views.CSV.make(data, times));
-      Array.prototype.forEach.call(ELS.csv, function(el) {
+      // set csv btn download names
+      MM.each(ELS.csv, function(el) {
         el.download = data.summary + '.csv';
-        el.href = 'data:text/csv;base64,' + csv;
       });
     });
 
-    // add event listeners
-    Array.prototype.forEach.call(INPUTS, function(el) {
+    // add event handlers to all fields
+    MM.each(ELS.fields, function(el) {
       el.addEventListener('change', refresh, false);
       el.addEventListener('keydown', refresh, false);
+    });
+
+    // add ics btn event handlers
+    MM.each(ELS.ics, function(el) {
+      el.addEventListener('click', function() {
+        // set calendar download button
+        var ics = btoa(MM.Views.ICS.make(CACHE.data, CACHE.evs));
+        el.href = 'data:text/calendar;base64,' + ics;
+      });
+    });
+
+    // add csv btn event handlers
+    MM.each(ELS.csv, function(el) {
+      el.addEventListener('click', function() {
+        var csv = btoa(MM.Views.CSV.make(CACHE.data, CACHE.evs));
+        el.href = 'data:text/csv;base64,' + csv;
+      });
     });
 
     // focus summary
